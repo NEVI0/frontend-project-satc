a3790a3aaa9fd94124d48f553f33564d32c50660
NEVI0 <neviocostamagagnin@gmail.com>
2024-06-19 21:44:22 -0300

    feat: adicionando nova property de ações no outlet content

--- src/components/OutletContent/index.tsx
+++ src/components/OutletContent/index.tsx
@@ -1,14 +1,36 @@
 import { FC, ReactNode } from 'react';
 import './styles.css';
 
+interface IActions {
+    text: string;
+    color: string;
+    onClick: () => void;
+}
+
 interface OutletContentProps {
     title: string;
+    actions?: IActions[];
     children: ReactNode;
 }
 
-export const OutletContent: FC<OutletContentProps> = ({ title, children }) => (
+export const OutletContent: FC<OutletContentProps> = ({ title, actions = [], children }) => (
     <section>
-        <h2>{title}</h2>
+        <div>
+            <h2>{title}</h2>
+
+            <div>
+                {!!actions.length && actions.map(action => (
+                    <button
+                        key={action.text}
+                        onClick={action.onClick}
+                        style={{ color: action.color }}
+                    >
+                        {action.text}
+                    </button>
+                ))}
+            </div>
+        </div>
+
         {children}
     </section>
 );

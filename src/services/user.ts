893840e1657bb9b921ddb649096824dc573dfd2f
NEVI0 <neviocostamagagnin@gmail.com>
2024-06-19 21:43:53 -0300

    feat: adicionando request de update e delete do usuário

--- src/services/user.ts
+++ src/services/user.ts
@@ -14,12 +14,15 @@ export const fetchUserByEmailService = async (email: string) => {
 }
 
 export const fetchUserByEmailAndPasswordService = async (email: string, password: string) => {
-    const response = await fetch(`${url}?email=${email}&password=${password}`);
+    const response = await fetch(`${url}?email=${email}`);
+    
     const users = await response.json() as IUser[];
-
     if (!users.length) return null;
-    delete users[0].password;
 
+    const incorrectPassword = users[0].password !== password;
+    if (incorrectPassword) return null;
+    
+    delete users[0].password;
     return users[0];
 }
 
@@ -36,4 +39,27 @@ export const addUserService = async (user: Omit<IUser, 'id'>) => {
     delete created.password;
 
     return created!;
+}
+
+export const updateUserService = async (user: IUser) => {
+    return await fetch(url + '/' + user.id, {
+        method: 'PUT',
+        body: JSON.stringify({
+            name: user.name,
+            email: user.email,
+            password: user.password,
+        }),
+        headers: {
+            'content-type': 'application/json'
+        },
+    });
+}
+
+export const deleteUserService = async (userId: IUser['id']) => {
+    return await fetch(url + '/' + userId, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json'
+        },
+    });
 }
